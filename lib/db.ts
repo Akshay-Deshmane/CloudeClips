792488705855e79ee101d59a39c25009c4dc2959
Akshay-Deshmane <akshaydeshmane1087@gmail.com>
2026-04-27 19:58:13 +0530

    DB-Setup

---
+++ lib/db.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const MONGO_URL = process.env.MONGO_URL!
+
+if(!MONGO_URL) {
+    throw new Error("Please define the MONGO_URL in env file");
+}
+
+let cached = global.mongoose;
+
+if(!cached) {
+    cached = global.mongoose = {
+        conn : null,
+        promise : null
+    }
+}
+
+export async function connectTODatabase() {
+    if(cached.conn) {
+        return cached.conn;
+    }
+
+    if(!cached.promise) {
+        const opts = {
+            bufferCommands : true,
+            maxPoolSize : 10
+        }
+
+        mongoose.connect(MONGO_URL, opts)
+        .then(() => mongoose.connection) 
+    }
+
+    try{
+        cached.conn = await cached.promise
+    }
+    catch(error) {
+        cached.promise = null;
+        throw error
+    }
+
+    return cached.conn;
+}
