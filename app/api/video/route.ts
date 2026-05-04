c4abefc145d1ea9526a227ddb7216ae688780444
Akshay-Deshmane <akshaydeshmane1087@gmail.com>
2026-05-04 20:09:52 +0530

    ImageKit-Setup

---
+++ app/api/video/route.ts
@@ -0,0 +1,81 @@
+import { authOptions } from "@/lib/auth";
+import { connectTODatabase } from "@/lib/db";
+import Video, { IVideo } from "@/models/video";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function GET() {
+
+    try{
+
+        await connectTODatabase();
+        const videos = await Video.find({}).sort({createdAt : -1}).lean();
+
+        if(!videos || videos.length === 0) {
+            return NextResponse.json([], {status : 200})
+        }
+
+        return NextResponse.json(videos);
+
+    }
+    catch(error) {
+
+        return NextResponse.json({
+            error : "Failed to Fetch the videos....."
+        }, {status : 500})
+        
+    }
+
+}
+
+
+export async function POST(request : NextRequest) {
+
+    try {
+       
+        const session = await getServerSession(authOptions);
+        
+        if(!session) {
+            return NextResponse.json(
+                {error : "Unauthorized User"},
+                {status : 401}
+            );
+        }
+
+        await connectTODatabase();
+
+        const body : IVideo = await request.json()
+
+        if(!body.title || !body.description || !body.videoUrl || !body.thumbnailUrl) {
+            return NextResponse.json(
+                {error : "Missing required fields"},
+                {status : 400}
+            );
+        }
+         
+        const videoData = {
+            ...body,
+            controls: body?.controls ?? true,
+            transformation : {
+                height : 1920,
+                width : 1080,
+                quality : body.transformation?.quality ?? 100,
+            },
+        };
+
+        const newVideo = await Video.create(videoData);
+
+        return NextResponse.json(newVideo);
+
+    }
+    catch(error) {
+
+        return NextResponse.json(
+            {error : "Failed to upload the video"},
+            {status : 500}
+        );
+
+    }
+
+}
