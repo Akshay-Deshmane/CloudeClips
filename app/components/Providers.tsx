8a144f666223dd4d457572b2f8c6f7e1d068cb1b
Akshay-Deshmane <akshaydeshmane1087@gmail.com>
2026-05-06 22:35:33 +0530

    Components-setup

---
+++ app/components/Providers.tsx
@@ -0,0 +1,15 @@
+import { ImageKitProvider } from "@imagekit/next";
+import { SessionProvider } from "next-auth/react";
+
+
+const urlEndPoint = process.env.NEXTAUTH_URL!;
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <SessionProvider refetchInterval={5 * 60}>
+        <ImageKitProvider urlEndpoint={urlEndPoint}>
+                {children}
+        </ImageKitProvider>
+    </SessionProvider>
+  );
+}
